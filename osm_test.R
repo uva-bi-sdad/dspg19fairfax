6d6cb0f72a3986805ad8f60f4ba948fcf28e4160
Cong <ccong2@illinois.edu>
2019-06-12 19:10:42 +0000

    create

--- osm_test.R
+++ osm_test.R
@@ -1,18 +1,28 @@
-install.packages("osmdata")
-library(osmdata)
-library(ggmap)
-
-# References:
-# https://cran.r-project.org/web/packages/osmdata/vignettes/osmdata.html
-# https://wiki.openstreetmap.org/wiki/Map_Features
-
-highway<-opq(bbox = 'fairfax county')%>%
-  add_osm_feature(key = 'boundary', value = 'administrative')%>%
-  osmdata_sp ()
-sp::plot(highway$osm_lines)
+#library(osmdata)
+library(rgdal)
+#library(sf)
+library(tidycensus)
+library(ggplot2)
 
 
-hospital<-opq(bbox = 'fairfax county')%>%
-  add_osm_feature(key = 'amenity', value = 'hospital')%>%
-  osmdata_sp ()
-sp::plot(hospital$osm_points,add=TRUE,pch=18,col="red")
+# Get Median Household Income from ACS and plot it
+census_api_key("7a7317f23e9ade546eb19fc64649727eccc17b24",overwrite=TRUE,install = TRUE)
+readRenviron("~/.Renviron")
+
+medincome<-get_acs(geography = "tract", county = "Fairfax County",
+                   variables = "B19013_001", 
+                   state='VA', geometry = TRUE)
+#head(medincome)
+medincome %>%
+  ggplot(aes(fill = estimate)) + 
+  geom_sf(color = NA) + 
+  coord_sf(crs = 26911) + 
+  scale_fill_viridis_c(option = "magma") 
+
+# OSM hospital data
+hospital<-opq('Fairfax County')%>%
+  add_osm_feature(key='amenity',value=c('hospital','baby_hatch','clinic','dentist','doctors',
+                                        'pharmacy','social_facility','veterinary'))%>%
+  osmdata_sf()
+
+proj4string(hospital$osm_points$geometry)
