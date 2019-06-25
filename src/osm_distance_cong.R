3b7c34e4e7506c4a40549fd8ca28b8c7ded2f7f2
Cong <ccong2@illinois.edu>
2019-06-25 18:10:10 +0000

    add scripts for supermarket travel ischrones

--- src/osm_distance_cong.R
+++ src/osm_distance_cong.R
@@ -1,33 +1,42 @@
 devtools::install_github("tlorusso/traveltime")
 
-# load packages
+# Load packages
 if (!require("pacman")) install.packages("pacman")
 pacman::p_load(dplyr, purrr, sf, mapview, traveltime)
 
-## Basic plotting
-traveltime30 <- traveltime_map(appId="appid",
-                               apiKey="appkey",
-                               location=c(38.842669,-77.270272),
+## Test basic plotting
+traveltime30 <- traveltime_map(appId="3c54476f",
+                               apiKey="3902eeb364e6db623d0b4f11fc25379c",
+                               location=c(38.842669, -77.270272),
                                traveltime=1800,
                                type="public_transport",
                                departure="2019-6-26T08:00:00Z")
-mapview(traveltime30,  col.regions = c("grey")) + traveltime30
+mapview(traveltime_smt,  col.regions = c("grey")) + traveltime_smt
 
 
-## Try OSM
+#===========THIS IS THE PART FOR FETCHING SUPERMARKETS TRAVEL POLYGONS==============
+
+## OSM Supermarket
 library(osmdata)
 supermarket<-opq('Fairfax County')%>%
   add_osm_feature(key='shop',value=c('supermarket'))%>%
   osmdata_sf()
 
-
 df<-as.data.frame(st_coordinates(supermarket$osm_points))
 
-df%>%mutate(sf=map2(.x=X,.y=Y,~traveltime_map(appId="appid",
-                                              apiKey="appkey",
-                                              location=c(.y,.x),
-                                              traveltime=1200,
-                                              type="public_transport",
-                                              departure="2019-6-26T08:00:00Z")))
+traveltime_smt<-data.frame()
+
+for (i in 1:nrow(df)){
+  this.one <- traveltime_map(appId="3c54476f",
+                                 apiKey="3902eeb364e6db623d0b4f11fc25379c",
+                                 location=c(df$Y[i], df$X[i]),
+                                 traveltime=1200,
+                                 type="public_transport",
+                                 departure="2019-6-26T08:00:00Z")
+  traveltime_smt<-rbind.data.frame(traveltime_smt,this.one)
+  Sys.sleep(5)
+}
+
+#========================================================================
 
 
