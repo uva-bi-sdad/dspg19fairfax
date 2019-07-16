ed7e8c7dbebee3bcda3667ec166440248b0ed463
Eliza Tobin <ent4pn@virginia.edu>
2019-07-16 16:41:04 +0000

    updated merging code for data

--- src/Eliza/07_11_2019_joined_acs_script.r
+++ src/Eliza/07_11_2019_joined_acs_script.r
@@ -4,4 +4,6 @@ library(tidyverse)
 acs.1 <- read.csv("./data/working/ACS_final_index/master_census_dataset.csv")
 acs.2 <- read.csv("./data/working/ACS_final_index_2/master_dataset_eliza.csv")
 acs.joined <- left_join(acs.1, acs.2, by = c("id", "id_type"))
-write.csv(acs.joined, "./data/working/ACS_final_index_2/07_11_2019_joined_acs_final.csv")
+##write.csv(acs.joined, "./data/working/ACS_final_index_2/07_16_2019_joined_acs_final.csv")
+
+
