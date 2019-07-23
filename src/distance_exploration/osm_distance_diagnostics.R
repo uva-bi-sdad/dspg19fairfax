8bdf30c6aaa0a15994f67fff9d92769131975f5d
Teja Pristavec <tp2sk@virginia.edu>
2019-07-23 11:57:10 +0000

    Added new isochrone diagnostics

--- src/distance_exploration/osm_distance_diagnostics.R
+++ src/distance_exploration/osm_distance_diagnostics.R
@@ -2,13 +2,15 @@ library(sf)
 library(dplyr)
 library(maps)
 library(readr)
+library(ggplot2)
+library(stringr)
 
 
-# "I think the polys are wrong" file
+########################### PART 1: "I think the polys are wrong" (10 minute drive time) ######################## 
 
 
 #
-# Prepare data --------------------------------------------------------------------------------------------------------
+# Prepare data --------------------------------------------------------------------------------------------------
 #
 
 # Read in Fairfax housing data
@@ -39,17 +41,60 @@ plot(st_geometry(test1[1:10, ]))
 plot(st_geometry(test2[1:10, ]), add = TRUE, col = "red", lwd = 20)
 
 # Get Virginia and Fairfax County maps to plot border
-va <- map_data("county", "virginia")
-va <- st_as_sf(va, coords = c("long", "lat"))
-
-unique(va$subregion)
-fairfax <- va %>% filter(subregion == "fairfax")
-ffxmap <- st_as_sf(fairfax, coords = c("long", "lat"))
+counties <- st_as_sf(map("county", plot = FALSE, fill = TRUE))
+counties <- counties %>% filter(str_starts(ID, "virginia"))
+counties <- counties %>% filter(str_starts(ID, "virginia,fairfax"))
 
 # Plot
-plot(st_geometry(ffxmap))
+plot(st_geometry(counties))
 plot(st_geometry(test1[1:10, ]), add = TRUE)
 plot(st_geometry(test2[1:10, ]), add = TRUE, col = "red", lwd = 10)
-title("10 minute drive time polygons from the first 10 alcohol shops in Fairfax County\n(supposedly)")
+title("10 minute drive time isochrones from the first 10 alcohol shops in Fairfax County\n(supposedly)")
+
+
+
+
+######################################## PART 2: Looks better (20 minute bus ride time) ######################## 
+
+
+#
+# Prepare data -------------------------------------------------------------------------------------------------
+#
+
+
+# Read in OSM multipolygons
+osm <- read_rds("./data/working/Time_distance_files/public_transport_osm_files/time_dist_sf.RDS")
+
+# Extract and convert test example (alcohol stores)
+test <- osm$data[[1]]
+
+
+#
+# Plot --------------------------------------------------------------------------------------------------------
+#
+
+# Get travel time polygons
+test1 <- st_sf(test)
+
+# Get original points
+test2 <- st_as_sf(test, coords = c("lng", "lat"))
+
+# Plot the first 10 travel time polygons with the corresponding first 10 original points
+plot(st_geometry(test1[1:10, ]))
+plot(st_geometry(test2[1:10, ]), add = TRUE, col = "red", lwd = 20)
+
+# Get Virginia and Fairfax County maps to plot border
+counties <- st_as_sf(map("county", plot = FALSE, fill = TRUE))
+counties <- counties %>% filter(str_starts(ID, "virginia"))
+counties <- counties %>% filter(str_starts(ID, "virginia,fairfax"))
+
+# Plot
+plot(st_geometry(counties))
+plot(st_geometry(test1[1:10, ]), add = TRUE)
+plot(st_geometry(test2[1:10, ]), add = TRUE, col = "red", pch = 19, cex = 0.5)
+title("20 minute bus ride time isochrones from the first 10 alcohol shops in Fairfax County")
+
+
+
 
 
